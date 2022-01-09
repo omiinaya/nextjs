40ca1dee08791f0d5937d214abcb96b21255ef49
omiinaya <omiinaya@gmail.com>
2022-01-08 19:39:01 -0500

    progress.

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,9 +1,15 @@
+import NavBar from '../NavBar/NavBar'
 import style from "./Home.module.scss";
 
 export default function Home() {
   return (
     <div className={style.root}>
+      <div className={style.NavBar}>
+        <NavBar />
+      </div>
+      <div>
         test
+      </div>
     </div>
   )
 }
