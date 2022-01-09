c1a95d4696e84f55fb5c036f405dc323af9f98af
omiinaya <omiinaya@gmail.com>
2022-01-09 03:34:37 -0500

    cleanup.

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -1,19 +1,11 @@
 import Typography from '@mui/material/Typography';
 import style from "./Skills.module.scss";
-import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
-import Slide from '@mui/material/Slide';
-import Fade from '@mui/material/Fade';
 import Breadcrumb from '../Breadcrumb/Breadcrumb'
 import CodeIcon from '@mui/icons-material/Code';
 import Test from '../Test/Test'
 
 export default function Skills() {
-    const [mainSlide, setMainSlide] = useState(false);
-
-    useEffect(() => {
-        setTimeout(() => { setMainSlide(true) }, 300);
-    });
 
     return (
         <Box>
@@ -21,35 +13,32 @@ export default function Skills() {
                 ml: { xs: 'none', sm: '50px', md: '70px', lg: '70px' },
                 mr: { xs: 'none', sm: '50px', md: '70px', lg: '70px' }
             }}>
-                <Fade in={mainSlide} >
-                    <Box className={style.breadcrumb}>
-                        <Breadcrumb current={'Skills'} />
-                    </Box>
-                </Fade>
+                <Box className={style.breadcrumb}>
+                    <Breadcrumb current={'Skills'} />
+                </Box>
             </Box>
             <Box sx={{
                 ml: { xs: 'none', sm: '50px', md: '70px', lg: '70px' },
                 mr: { xs: 'none', sm: '50px', md: '70px', lg: '70px' }
             }}>
-                <Slide direction="up" in={mainSlide} className={style.root}>
-                    <Box>
-                        <Box
-                            display="flex"
-                            justifyContent="center"
-                        >
-                            <Typography textAlign="center" className={style.title}>
-                                .Skills 
-                            </Typography>
-                            <CodeIcon sx={{
-                                height: '70px',
-                                width: '70px',
-                                ml: '20px'
-                            }}
-                            />
-                        </Box>
-                        <Test />
+                <Box className={style.root}>
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                    >
+                        <Typography textAlign="center" className={style.title}>
+                            .Skills
+                        </Typography>
+                        <CodeIcon sx={{
+                            height: '70px',
+                            width: '70px',
+                            ml: '20px'
+                        }}
+                        />
                     </Box>
-                </Slide>
+                    test
+                </Box>
+
             </Box>
         </Box>
     )
