cb15158012741cfe8d83955bdea92f0102c08ee6
Omar Minaya <omiinaya@gmail.com>
2022-01-10 04:36:15 -0500

    .

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import NavBar from '../components/NavBar'
 import Home from '../components/Home'
 import Skills from '../components/Skills'
 import Box from '@mui/material/Box';
+//import { useState } from 'react';
+//import VisibilitySensor from 'react-visibility-sensor';
 
 export default function App() {
+  //const [tab, setTab] = useState(false)
   return (
     <Box>
       <Head>
