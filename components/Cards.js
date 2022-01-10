d23b851bf50cca9a0afcaf2793e4aca337517f1e
Omar Minaya <omiinaya@gmail.com>
2022-01-10 03:25:55 -0500

    cards progress.

--- components/Cards.js
+++ components/Cards.js
@@ -2,40 +2,26 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grow from '@mui/material/Grow';
 
-const icon = (
-    <Paper sx={{ m: 1 }} elevation={4}>
-        <Box component="svg" sx={{ width: 100, height: 100 }}>
-            <Box
-                component="polygon"
-                sx={{
-                    fill: (theme) => theme.palette.common.white,
-                    stroke: (theme) => theme.palette.divider,
-                    strokeWidth: 1,
-                }}
-                points="0,100 50,00, 100,100"
-            />
-        </Box>
-    </Paper>
-);
-
-const cards = ['JavaScript', 'React', 'HTML', 'CSS']
-
-export default function SimpleGrow(props) {
+export default function test(props) {
     return (
-        <Box m={'60px'} >
-            <Box sx={{ display: 'flex' }}>
-                {cards.map((card, index) => (
-                    <Grow
-                        in={props.visible}
-                        key={card}
-                        style={{ transformOrigin: '0 0 0' }}
-                        {...(props.visible ? { timeout: (index+1) * 500 } : {})}
-                    >
-                        {icon}
-                    </Grow>
-                ))}
-
-            </Box>
+        <Box sx={{ display: 'flex' }}>
+            {props.cards.map((card, index) => (
+                <Grow
+                    in={props.visible}
+                    key={card}
+                    style={{
+                        //transformOrigin: '0 0 0',
+                        transform: `translateY(${(index + 0) * 30}px)`
+                    }}
+                    {...(props.visible ? { timeout: (index + 1) * 500 } : {})}
+                >
+                    <Paper sx={{ m: 1 }} elevation={4}>
+                        <Box sx={{ width: 170, height: 200 }}>
+                            tests
+                        </Box>
+                    </Paper>
+                </Grow>
+            ))}
         </Box>
-    );
+    )
 }
--- components/Skills.js
+++ components/Skills.js
@@ -1,21 +1,60 @@
 import { useState } from 'react';
 import Box from '@mui/material/Box';
-import Cards from './Cards'
+import Cards from './Cards';
 import VisibilitySensor from 'react-visibility-sensor';
 
 export default function SimpleGrow() {
     const [visible, setVisible] = useState(false);
+    const [visible2, setVisible2] = useState(false);
+    const [visible3, setVisible3] = useState(false);
+    const [visible4, setVisible4] = useState(false);
+    const cards1 = ['JavaScript', 'Node', 'React', 'HTML', 'CSS']
+    const cards2 = ['MySQL', 'Sequelize', 'MongoDB', 'MSSQL', 'JSON']
+    const cards3 = ['NextJS', 'React-Native', 'JQuery', 'Bootstrap', 'MDB']
+    const cards4 = ['Python', 'Java', 'Electron', 'Git', 'Express']
 
     return (
-        <Box 
-            display='flex'
-            justifyContent='center'
-        >
-            <VisibilitySensor
-                onChange={(e) => { setVisible(e) }}
+        <Box>
+            <Box
+                display='flex'
+                justifyContent='center'
             >
-                <Cards visible={visible} />
-            </VisibilitySensor>
+                <VisibilitySensor
+                    onChange={(e) => { if (e === true) setVisible(e) }}
+                >
+                    <Cards cards={cards1} visible={visible} />
+                </VisibilitySensor>
+            </Box>
+            <Box
+                display='flex'
+                justifyContent='center'
+            >
+                <VisibilitySensor
+                    onChange={(e) => { if (e === true) setVisible2(e) }}
+                >
+                    <Cards cards={cards2} visible={visible2} />
+                </VisibilitySensor>
+            </Box>
+            <Box
+                display='flex'
+                justifyContent='center'
+            >
+                <VisibilitySensor
+                    onChange={(e) => { if (e === true) setVisible3(e) }}
+                >
+                    <Cards cards={cards3} visible={visible3} />
+                </VisibilitySensor>
+            </Box>
+            <Box
+                display='flex'
+                justifyContent='center'
+            >
+                <VisibilitySensor
+                    onChange={(e) => { if (e === true) setVisible4(e) }}
+                >
+                    <Cards cards={cards4} visible={visible4} />
+                </VisibilitySensor>
+            </Box>
         </Box>
     );
 }
