e454c3d53f4f4b5e56b386cf580658222bda9c2f
Omar Minaya <omiinaya@gmail.com>
2022-01-13 21:57:16 -0500

    progress on projects.

--- components/Project.js
+++ components/Project.js
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 export default function Project(props) {
     console.log(props)
@@ -6,15 +7,35 @@ export default function Project(props) {
         <Box>
             {props.projects.map((project, index) => (
                 <div key={project.name}>
-                    <Box className="ProjectContainer">
+                    <Box className="ProjectContainer"
+                        display='flex'
+                        justifyContent='center'
+                    >
                         <Box className="Project">
-                            {'Source: '}
-                            <a
-                                href={project.html_url}
-                                target="_blank"
-                                rel="noreferrer">
+                            <Box>
+                                {'Image'}
+                            </Box>
+                            <Box>
                                 {project.name}
-                            </a>
+                            </Box>
+                            <Box>
+                                <a
+                                    href={project.html_url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    style={{ marginRight: '7px'}}
+                                >
+                                    <Button color='primary' variant="contained">View Source</Button>
+                                </a>
+                                <a
+                                    href={project.html_url}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    style={{ marginLeft: '7px'}}
+                                >
+                                    <Button color='primary' variant="contained">Deployment</Button>
+                                </a>
+                            </Box>
                         </Box>
                     </Box>
                 </div>
